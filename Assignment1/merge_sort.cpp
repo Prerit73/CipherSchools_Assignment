d0c7fb9bc90938acda7cc2c680a4850adf414519
prerit <prerit197@gmail.com>
2021-02-17 13:10:04 +0530

    Assignment1

--- Assignment1/merge_sort.cpp
+++ Assignment1/merge_sort.cpp
@@ -1,72 +1,78 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
+void merge(int arr[], int left, int mid, int right)
+{
 
-void merge(int arr[], int p, int q, int r) {
-  
-  int n1 = q - p + 1;
-  int n2 = r - q;
+  int n1 = mid - left + 1;
+  int n2 = right - mid;
 
   int L[n1], M[n2];
 
   for (int i = 0; i < n1; i++)
-    L[i] = arr[p + i];
+  {
+    L[i] = arr[left + i];
+  }
+
   for (int j = 0; j < n2; j++)
-    M[j] = arr[q + 1 + j];
+  {
+    M[j] = arr[mid + 1 + j];
+  }
 
-  int i, j, k;
-  i = 0;
-  j = 0;
-  k = p;
+  int i = 0, j = 0, k = left;
 
-  
-  while (i < n1 && j < n2) {
-    if (L[i] <= M[j]) {
+  while (i < n1 && j < n2)
+  {
+    if (L[i] <= M[j])
+    {
       arr[k] = L[i];
       i++;
-    } else {
+    }
+    else
+    {
       arr[k] = M[j];
       j++;
     }
     k++;
   }
 
-  while (i < n1) {
+  while (i < n1)
+  {
     arr[k] = L[i];
     i++;
     k++;
   }
 
-  while (j < n2) {
+  while (j < n2)
+  {
     arr[k] = M[j];
     j++;
     k++;
   }
 }
 
-void mergeSort(int arr[], int l, int r) {
-  if (l < r) {
-    int m = l + (r - l) / 2;
+void mergeSort(int arr[], int left, int right)
+{
+  if (left < right)
+  {
+    int mid = left + (right - left) / 2;
 
-    mergeSort(arr, l, m);
-    mergeSort(arr, m + 1, r);
+    mergeSort(arr, left, mid);
+    mergeSort(arr, mid + 1, right);
 
-    merge(arr, l, m, r);
+    merge(arr, left, mid, right);
   }
 }
 
+int main()
+{
+  int arr[]= {6, 5, 12, 10, 9, 1};
+  int n = 6;
 
-void printArray(int arr[], int size) {
-  for (int i = 0; i < size; i++)
-    cout << arr[i] << " ";
-}
+  mergeSort(arr, 0, n - 1);
 
-int main() {
-  int arr[] = {6, 5, 12, 10, 9, 1};
-  int size = sizeof(arr) / sizeof(arr[0]);
-
-  mergeSort(arr, 0, size - 1);
-
-  cout << "Sorted array "<<endl;
-  printArray(arr, size);
+  cout << "Sorted array " << endl;
+  for(int i:arr){
+    cout<<i<<" ";
+  }
 }
