82a1eb46ca909da6f79c20a4343b4b0601ecb53d
Prerit73 <57189948+Prerit73@users.noreply.github.com>
2021-07-16 00:21:43 +0530

    Update possible_permutation.cpp

--- Assignment2/possible_permutation.cpp
+++ Assignment2/possible_permutation.cpp
@@ -1,21 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void permutation(string s, string ans)
-{
-    if (s.length() == 0)
-    {
-        cout << ans << endl;
-        return;
+void generatePermutationHelper(string &str, int start, int end, vector<string>& result) {
+    if (start >= end) {
+        result.push_back(str);
+    } else {
+        for (int i = start; i <= end; i++) {
+            swap(str[i], str[start]);
+            generatePermutationHelper(str, start+1, end, result);
+            swap(str[i], str[start]);
+        }
     }
-    for (int i = 0; i < s.length(); i++)
-    {
-        char ch = s[i];
-        string rest = s.substr(0, i) + s.substr(i + 1);
-        permutation(rest, ans + ch);
-    }
+}
+
+vector<string> generatePermutation(string str) {
+    vector<string> result;
+    generatePermutationHelper(str, 0, str.size() - 1, result); 
+    return result;
 }
 int main()
 {
-    permutation("ABC", "");
+    vector<string>ans=generatePermutation("ABC", "");
 }
